1d9e2b57443fd07598e5916e9d78dd58c73cfeb4
Ростислав Жуківський <Coderhedgehog1993@outlook.com>
2020-02-08 00:37:39 -0800

    Updated

---
+++ Check the OS/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Check_the_OS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            GetOSVersion();
+        }
+        public void GetOSVersion()
+        {
+            int _MajorVersion = Environment.OSVersion.Version.Major;
+
+            if (_MajorVersion == 5)
+            {
+                label1.Text = "XP";
+                label2.Text = (_MajorVersion).ToString();
+            }
+            else
+            {
+                label1.Text = "NOT XP";
+                label2.Text = (_MajorVersion).ToString();
+            }
+        }
+    }
+}
